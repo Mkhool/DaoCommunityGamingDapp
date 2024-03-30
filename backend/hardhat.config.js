b36239a64792e239d68f8ce41617c2c5565582ed
medhi.khoule <khoule.medhi@hotmail.fr>
2024-03-30 15:26:08 +0100

    ajout Natspec et docgen

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("hardhat-docgen");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  docgen:{
+    path: "./docs",
+    clear: true,
+    runOnCompile:true
+  }
+
 };
